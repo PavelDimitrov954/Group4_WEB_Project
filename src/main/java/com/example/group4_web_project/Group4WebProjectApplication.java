188bc12d1fbd955e563d2e6c63bb278e2a6467e4
PavelDimitrov954 <miant@abv.bg>
2023-10-13 17:21:50 +0300

    First Upload
    Uploaded Spring Framework files

---
+++ src/main/java/com/example/group4_web_project/Group4WebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.group4_web_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group4WebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group4WebProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/group4_web_project/Group4WebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.group4_web_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group4WebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
