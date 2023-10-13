44d6f6a57fb9814b4b823fa76393834c380dc9f7
PavelDimitrov954 <miant@abv.bg>
2023-10-13 18:37:57 +0300

    Update

--- src/main/java/com/example/group4_web_project/config/HibernateConfig.java
+++ src/main/java/com/example/group4_web_project/config/HibernateConfig.java
@@ -28,7 +28,7 @@ public class HibernateConfig {
     public LocalSessionFactoryBean sessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan("com.company.web.springdemo.models");
+        sessionFactory.setPackagesToScan("com.example.group4_web_project.config");
         sessionFactory.setHibernateProperties(hibernateProperties());
         return sessionFactory;
     }
@@ -49,7 +49,7 @@ public class HibernateConfig {
         hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MariaDBDialect");
 
 //         Configure code-first capabilities
-        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+//        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
 
         return hibernateProperties;
     }
